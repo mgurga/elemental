18d070041afe8494f7dbfe30f3ff9f892d177796
rokie95 <motiurga@gmail.com>
2020-04-08 06:12:27 -0700

    roughly finished both server and client
    -client: completely new UI using blessed
    -client: continually polls server for new messages
    -server: optional usage contraints for messages added
    -server: optionally send clients new message
    -server: UserClient class holds more variables
    -both: lots of new commands added
    -both: lots of big changes

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
+
 import socket
 import json
 import datetime
 import getpass
+import blessed
+import time
+from threading import Thread
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
@@ -14,6 +18,16 @@ usernm = ""
 passwd = ""
 currentserver = ""
 currentchannel = ""
+providername = ""
+providerwelcome = ""
+currentchannelmsgnum = -1
+totalmessagesinchannel = -1
+messagestoload = 15
+
+terminalguimode = True
+awaitingmessage = False
+
+term = blessed.Terminal()
 
 serversocket.connect((host, port))
 
@@ -48,58 +62,292 @@ while True:
 			print("registration failed, " + regrespjson["reason"])
 			regORlog = ""
 	else:
+		print("invalid option")
 		regORlog = ""
 
-while True:
-	usermsg = input(currentserver + "/" + currentchannel + ": ")
-	if usermsg == "/createserver":
-		newservername = input("new server name: ")
-		newserverpass = input("new server password: ")
+getprovidernm = json.loads("{}")
+getprovidernm["call"] = "getprovidername"
+getprovidernm["usernm"] = usernm
+getprovidernm["passwd"] = passwd
+getprovidertext = json.dumps(getprovidernm)
+serversocket.send(getprovidertext.encode(encoding))
 
-		newserverjson = json.loads("{}")
-		newserverjson["call"] = "createserver"
-		newserverjson["name"] = newservername
-		newserverjson["passwd"] = newserverpass
-		newserverjson["owner"] = usernm
-		serversocket.send((json.dumps(newserverjson)).encode(encoding))
+providerrespsock = serversocket.recv(4096)
+providerresp = json.loads(providerrespsock.decode(encoding))
 
-		servresponse = serversocket.recv(4096)
-		servresponsejson = json.loads(servresponse.decode(encoding))
+if providerresp["resp"] == True:
+	providername = providerresp["name"]
+	providerwelcome = providerresp["welcome"]
 
-		if servresponsejson["resp"] == True:
-			print("server created")
-			currentserver = newservername
-			currentchannel = "general"
-		else:
-			print("server not created, " + servresponsejson.reason)
-	
-	elif usermsg == "/deleteserver":
-		delservername = input("server name: ")
-		delserverpasswd = input("server passwd: ")
+keyboardbuffer = ""
+messagebuffer = [providerwelcome]
 
-		delserverjson = json.loads("{}")
-		delserverjson["call"] = "deleteserver"
-		delserverjson["servername"] = delservername
-		delserverjson["serverpasswd"] = delserverpasswd
-		delserverjson["usernm"] = usernm
-		delserverjson["passwd"] = passwd
-		serversocket.send((json.dumps(delserverjson)).encode(encoding))
+def drawterminal():
+	print(term.home + term.clear)
 
-		servresponse = serversocket.recv(4096)
-		servresponsejson = json.loads(servresponse.decode(encoding))
+	while len(messagebuffer) > term.height - 5:
+		del messagebuffer[0]
 
-		if servresponsejson["resp"] == True:
-			print("server successfully deleted")
-			if currentserver == delservername:
-				currentserver = ""
-			currentchannel = ""
-		else:
-			print("server not deleted, " + servresponsejson["reason"])
+	if currentserver == "" and currentchannel == "":
+		connectedtodisplay = "not connected"
 	else:
-		usermsgjson = '{"call": "message", "msg": "' + usermsg + '"}'
-		serversocket.send(usermsgjson.encode(encoding))
+		connectedtodisplay = currentserver + "/" + currentchannel
+		
+	print(term.move_xy(0, 0) + providername + ", " + connectedtodisplay + ", loaded messages up to: " + str(currentchannelmsgnum) + "(" + str(totalmessagesinchannel) + " latest)")
+	print(term.move_xy(0, 1) + ("-" * term.width))
 
-		servresponse = serversocket.recv(4096)
-		print("server responded with: " + servresponse.decode(encoding))
+	for i in range(0, len(messagebuffer)):
+		print(term.move_xy(0, i + 2) + messagebuffer[i])
+	
+	print(term.move_xy(0, term.height - 3) + ("-" * term.width))
+	print(term.move_xy(0, term.height - 2) + usernm + ": " + keyboardbuffer)
+
+drawterminal()
+
+def updateMessages():
+	while True:
+		if not (currentserver == "" or currentchannel == ""):
+			messagetotal = json.loads("{}")
+			messagetotal["call"] = "gettotalmessages"
+			messagetotal["servername"] = currentserver
+			messagetotal["channel"] = currentchannel
+			messagetotal["usernm"] = usernm
+			messagetotal["passwd"] = passwd
+
+			serversocket.send((json.dumps(messagetotal)).encode(encoding))
+			servresponse = serversocket.recv(4096)
+			servresponsejson = json.loads(servresponse.decode(encoding))
+
+			if servresponsejson["resp"] == True:
+				begin = 0
+				end = 0
+				global currentchannelmsgnum
+				global totalmessagesinchannel
+
+				totalmessagesinchannel = servresponsejson["amount"] 
+				if currentchannelmsgnum == -1:
+					if servresponsejson["amount"] < messagestoload:
+						begin = 0
+					else:
+						begin = servresponsejson["amount"] - messagestoload
+					end = servresponsejson["amount"]
+				elif currentchannelmsgnum < servresponsejson["amount"]:
+					begin = currentchannelmsgnum
+					end = servresponsejson["amount"]
+
+				if not (begin == 0 or end == 0):
+					messagetotal = json.loads("{}")
+					messagetotal["call"] = "getmessages"
+					messagetotal["servername"] = currentserver
+					messagetotal["channel"] = currentchannel
+					messagetotal["begin"] = begin
+					messagetotal["end"] = end
+					messagetotal["usernm"] = usernm
+					messagetotal["passwd"] = passwd
+
+					serversocket.send((json.dumps(messagetotal)).encode(encoding))
+					servresponse = serversocket.recv(4096)
+					servresponsejson = json.loads(servresponse.decode(encoding))
+
+					if servresponsejson["resp"] == True:
+						messages = servresponsejson["messages"]
+
+						for i in range(0, len(messages)):
+							messagebuffer.append(messages[i]["author"] + ": " + messages[i]["message"])
+
+						#log("messages up to date")
+						currentchannelmsgnum = end
+
+						drawterminal()
+					else:
+						err("unable to get messages, " + servresponsejson["reason"])
+
+			else:
+				err("unable to get total messages, " + servresponsejson["reason"])
+		time.sleep(0.75)
+		
+def log(msg):
+	if terminalguimode == True:
+		messagebuffer.append(msg)
+		drawterminal()
+	else:
+		print(msg)
+
+def err(err):
+	if terminalguimode == True:
+		messagebuffer[len(messagebuffer) - 1] = messagebuffer[len(messagebuffer) - 1] + "  !!!" + err + "!!!"
+		drawterminal()
+	else:
+		print(err)
+
+messageupdater = Thread(target=updateMessages)
+messageupdater.daemon = True
+messageupdater.start()
+
+while True:
+	#usermsg = input(currentserver + "/" + currentchannel + ": ")
+	usermsg = ""
+
+	with term.cbreak():
+		inputkey = term.inkey()
+		if inputkey.code == 343:
+			messagebuffer.append(usernm + ": " + keyboardbuffer)
+			usermsg = keyboardbuffer
+			keyboardbuffer = ""
+			
+		elif inputkey.code == 263:
+			keyboardbuffer = keyboardbuffer[0:(len(keyboardbuffer) - 1)]
+		else:
+			keyboardbuffer += inputkey
+
+	drawterminal()
+
+	try:
+		if usermsg == "/createserver":
+			newservername = input("new server name: ")
+			newserverpass = input("new server password: ")
+
+			newserverjson = json.loads("{}")
+			newserverjson["call"] = "createserver"
+			newserverjson["name"] = newservername
+			newserverjson["serverpasswd"] = newserverpass
+			#newserverjson["owner"] = usernm
+			newserverjson["usernm"] = usernm
+			newserverjson["passwd"] = passwd
+			serversocket.send((json.dumps(newserverjson)).encode(encoding))
+
+			servresponse = serversocket.recv(4096)
+			servresponsejson = json.loads(servresponse.decode(encoding))
+
+			if servresponsejson.get("call") == None:
+				if servresponsejson["resp"] == True:
+					log("server created")
+					currentserver = newservername
+					currentchannel = "general"
+				else:
+					log("server not created, " + servresponsejson["reason"])
+		
+		elif usermsg == "/deleteserver":
+			delservername = input("server name: ")
+			delserverpasswd = input("server passwd: ")
+
+			delserverjson = json.loads("{}")
+			delserverjson["call"] = "deleteserver"
+			delserverjson["servername"] = delservername
+			delserverjson["serverpasswd"] = delserverpasswd
+			delserverjson["usernm"] = usernm
+			delserverjson["passwd"] = passwd
+
+			serversocket.send((json.dumps(delserverjson)).encode(encoding))
+
+			servresponse = serversocket.recv(4096)
+			servresponsejson = json.loads(servresponse.decode(encoding))
+
+			if servresponsejson.get("call") == None:
+				if servresponsejson["resp"] == True:
+					log("server successfully deleted")
+					if currentserver == delservername:
+						currentserver = ""
+					currentchannel = ""
+				else:
+					log("server not deleted, " + servresponsejson["reason"])
+		elif usermsg == "/listservers":
+			log("listing servers...")
+
+			listrequest = json.loads("{}")
+			listrequest["call"] = "getjoinedservers"
+			listrequest["usernm"] = usernm
+			listrequest["passwd"] = passwd
+
+			serversocket.send((json.dumps(listrequest)).encode(encoding))
+
+			servresponse = serversocket.recv(4096)
+			servresponsejson = json.loads(servresponse.decode(encoding))
+
+			if servresponsejson.get("call") == None:
+				if servresponsejson["resp"] == True:
+					servers = servresponsejson["servers"]
+					log(" ".join(servers))
+				else:
+					err("couldn't list servers, " + servresponsejson["reason"])
+
+		elif usermsg == "/joinserver":
+			
+			servername = input("input server name: ")
+			serverpasswd = input("input server password: ")
+
+			joinrequest = json.loads("{}")
+			joinrequest["call"] = "joinserver"
+			joinrequest["servername"] = servername
+			joinrequest["serverpasswd"] = serverpasswd
+			joinrequest["usernm"] = usernm
+			joinrequest["passwd"] = passwd
+
+			serversocket.send((json.dumps(joinrequest)).encode(encoding))
+
+			servresponse = serversocket.recv(4096)
+			servresponsejson = json.loads(servresponse.decode(encoding))
+
+			if servresponsejson.get("call") == None:
+				if servresponsejson["resp"] == True:
+					log("successfully joined server")
+				else:
+					err("unable to join server, " + servresponsejson["reason"])
+
+		elif usermsg == "/gotoserver":
+			gotoserver = input("target server: ")
+
+			listrequest = json.loads("{}")
+			listrequest["call"] = "getjoinedservers"
+			listrequest["usernm"] = usernm
+			listrequest["passwd"] = passwd
+
+			serversocket.send((json.dumps(listrequest)).encode(encoding))
+
+			servresponse = serversocket.recv(4096)
+			servresponsejson = json.loads(servresponse.decode(encoding))
+
+			if servresponsejson.get("call") == None:
+				if servresponsejson["resp"] == True:
+					if gotoserver in servresponsejson["servers"]:
+						log("entered server and joined #general")
+						currentserver = gotoserver
+						currentchannel = "general"
+						currentchannelmsgnum = -1
+						messagebuffer = []
+					else:
+						err("not apart of target server")
+				else:
+					err("couldn't get list of servers, " + servresponsejson["reason"])
+		
+		elif len(usermsg) > 0 and usermsg[0] == "/":
+			log("command not recognized")
+
+		elif usermsg != "":
+			usermsgjson = json.loads("{}")
+			usermsgjson["call"] = "message"
+			usermsgjson["msg"] = usermsg
+			usermsgjson["servername"] = currentserver
+			usermsgjson["channel"] = currentchannel
+			usermsgjson["usernm"] = usernm
+			usermsgjson["passwd"] = passwd
+
+			serversocket.send((json.dumps(usermsgjson)).encode(encoding))
+
+			servresponse = serversocket.recv(4096)
+			servresponsejson = json.loads(servresponse.decode(encoding))
+
+			if servresponsejson.get("call") == None:
+				if not servresponsejson["resp"] == True:
+					err(servresponsejson["reason"])
+				else:
+					currentchannelmsgnum = currentchannelmsgnum + 1
+					totalmessagesinchannel = totalmessagesinchannel + 1
+					drawterminal()
+	
+	except KeyError as e:
+		print("error")
+		print(e.with_traceback())
+
 
 serversocket.close()
--- elemental.py
+++ elemental.py
@@ -18,9 +18,15 @@ args = parser.parse_args()
 host = args.host
 port = args.port
 encoding = args.encoding
+providername = "LOCALPROVIDER"
+providerwelcome = "welcome to your LOCALPROVIDER, the BEST test provider on localhost"
+
 serverStorage = "serverstorage"
-serverName = "[SERVER]"
+serverNameInConsole = "[SERVER]" # in console
 jsonfileindent = 2
+sendotherusersdata = False
+
+clients = []
 
 if not os.path.exists(serverStorage):
 	os.mkdir(serverStorage)
@@ -31,19 +37,33 @@ if not os.path.exists(serverStorage):
 
 def new_client(clientsocket,addr):
 	client = UserClient(clientsocket, addr)
+	clients.append(client)
+	print("total clients: " + str(len(clients)))
 	while True:
 		try:
 			clientmsg = clientsocket.recv(4096)
-			print("(" + getTime() + ") | " + serverName + " <== [" + str(addr[0]) + ":" + str(addr[1]) + "] | " + clientmsg.decode(encoding))
+			print("(" + getTime() + ") | " + serverNameInConsole + " <== [" + str(addr[0]) + ":" + str(addr[1]) + "] | " + clientmsg.decode(encoding))
 			resp = client.rawcommand(clientmsg.decode(encoding))
-			print("(" + getTime() + ") | " + serverName + " ==> [" + str(addr[0]) + ":" + str(addr[1]) + "] | " + resp)
+			print("(" + getTime() + ") | " + serverNameInConsole + " ==> [" + str(addr[0]) + ":" + str(addr[1]) + "] | " + resp)
 			clientsocket.send(resp.encode(encoding))
-		except socket.error:
-			print("(" + getTime() + ") | closing connection because of error")
+		except socket.error as e:
+			print("(" + getTime() + ") | closing connection because of socket error, details below")
+			print(e)
 			break
+	clients.remove(client)
 	client.clientsocket.close()
+	print("total clients: " + str(len(clients)))
 
 class UserClient:
+
+	usernm = ""
+	passwd = ""
+	joinedservers = []
+	latestmessagejson = json.loads("{}")
+	latestmessageserver = ""
+	lastgetmessagerequest = datetime.datetime.now().timestamp()
+	lastgetmessagetotalrequest = datetime.datetime.now().timestamp()
+
 	def __init__(self, clientsocket, addr):
 		self.clientsocket = clientsocket
 		self.addr = addr
@@ -69,6 +89,9 @@ class UserClient:
 		if userfilejson["passwd"] == passwd:
 			self.putjson(userfilejson, serverStorage + os.sep + "users" + os.sep + usernm)
 			userfile.close()
+			self.usernm = usernm
+			self.passwd = passwd
+			self.joinedservers = userfilejson["joinedservers"]
 			return '{"resp": true}'
 		else:
 			return '{"resp": false, "reason":"password is incorrect"}'
@@ -105,14 +128,16 @@ class UserClient:
 
 		self.putjson(serverinfojson, serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info")
 
+		channelmessagesjson = json.loads('{}')
+		channelmessagesjson["messages"] = 0
+
+		self.putjson(channelmessagesjson, serverStorage + os.sep + "servers" + os.sep + servername + os.sep + name + os.sep + "messages")
+
 		return '{"resp": true}'
 
 	def joinserver(self, servername, serverpasswd, usernm):
 		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info"):
 			return '{"resp": false, "reason":"server does not exist"}'
-
-		if not os.path.exists(serverStorage + os.sep + "users" + os.sep + usernm):
-			return '{"resp": false, "reason":"user does not exist"}'
 
 		serverinfo = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info")
 		userfile = self.openjson(serverStorage + os.sep + "users" + os.sep + usernm)
@@ -129,18 +154,10 @@ class UserClient:
 		return '{"resp": true}'
 
 	def deleteserver(self, servername, serverpasswd, usernm, passwd):
-		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername):
+		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info"):
 			return '{"resp": false, "reason":"server does not exist"}'
 
-		if not os.path.exists(serverStorage + os.sep + "users" + os.sep + usernm):
-			return '{"resp": false, "reason":"user does not exist"}'
-
 		serverinfo = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info")
-		userinfo = self.openjson(serverStorage + os.sep + "users" + os.sep + usernm)
-
-		if not userinfo["passwd"] == passwd:
-			return '{"resp": false, "reason":"user password is incorrect"}'
-
 		serverusers = serverinfo["users"]
 
 		for i in range(0, len(serverusers)):
@@ -152,18 +169,11 @@ class UserClient:
 		return '{"resp": true}' 
 
 	def leaveserver(self, servername, usernm, passwd):
-		if not os.path.exists(serverStorage + os.sep + "users" + os.sep + usernm):
-			return '{"resp": false, "reason":"user does not exist"}'
-		
-		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername):
+		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info"):
 			return '{"resp": false, "reason":"server does not exist"}'
 
 		userinfo = self.openjson(serverStorage + os.sep + "users" + os.sep + usernm)
 		serverinfo = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info")
-
-		if not userinfo["passwd"] == passwd:
-			return '{"resp": false, "reason":"user password is incorrect"}'
-
 		userinfo["joinedservers"].remove(servername)
 		serverinfo["users"].remove(usernm)
 
@@ -173,7 +183,104 @@ class UserClient:
 		return '{"resp": true}'
 
 	def message(self, msg, servername, channel, usernm, passwd):
+		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info"):
+			return '{"resp": false, "reason":"server does not exist"}'
+
+		serverinfo = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info")
+		channelmessages = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + channel + os.sep + "messages")
+
+		if not channel in serverinfo["channels"]:
+			return '{"resp": false, "reason":"channel does not exist"}'
+
+		channelmessages["messages"] = channelmessages["messages"] + 1
+
+		self.putjson(channelmessages, serverStorage + os.sep + "servers" + os.sep + servername + os.sep + channel + os.sep + "messages")
+
+		messagejson = json.loads("{}")
+		messagejson["message"] = msg
+		messagejson["timestamp"] = datetime.datetime.now().timestamp()
+		messagejson["author"] = usernm
+
+		self.putjson(messagejson, serverStorage + os.sep + "servers" + os.sep + servername + os.sep + channel + os.sep + str(channelmessages["messages"] - 1))
+
+		messagejson["call"] = "newmessage"
+		
+		self.latestmessagejson = messagejson
+		self.latestmessageserver = servername
+
+		if sendotherusersdata:
+			self.sendotherusers(self.latestmessagejson, self.latestmessageserver)
+
 		return '{"resp": true}'
+
+	def getmessages(self, servername, channel, begin, end, usernm):
+		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info"):
+			return '{"resp": false, "reason":"server does not exist"}'
+		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + channel + os.sep + "messages"):
+			return '{"resp": false, "reason":"channel does not exist"}'
+		if not type(begin) is int:
+			return '{"resp": false, "reason":"begin is not an int"}'
+		if not type(end) is int:
+			return '{"resp": false, "reason":"end is not an int"}'
+		if end < begin:
+			return '{"resp": false, "reason":"begin is greater than end"}'
+		if end < 0 or begin < 0:
+			return '{"resp": false, "reason":"end or begin is less than 0"}'
+
+		channelmessages = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + channel + os.sep + "messages")
+		serverinfo = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + "info")
+
+		if end > channelmessages["messages"] or begin > channelmessages["messages"]:
+			return '{"resp": false, "reason":"end or begin is less than 0"}'
+		if not usernm in serverinfo["users"]:
+			return '{"resp": false, "reason":"user not in server"}'
+
+		outjson = json.loads("{}")
+		outjson["resp"] = True
+		outjson["messages"] = []
+
+		for i in range(0, end - begin):
+			messagedata = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + channel + os.sep + str(begin + i))
+			outjson["messages"].append(messagedata)
+
+		return json.dumps(outjson)
+
+	def gettotalmessages(self, servername, channel):
+		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername):
+			return '{"resp": false, "reason":"server does not exist"}'
+		if not os.path.exists(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + channel + os.sep + "messages"):
+			return '{"resp": false, "reason":"channel does not exist"}'
+
+		channelmessages = self.openjson(serverStorage + os.sep + "servers" + os.sep + servername + os.sep + channel + os.sep + "messages")
+
+		outjson = json.loads("{}")
+		outjson["resp"] = True
+		outjson["amount"] = channelmessages["messages"]
+
+		return json.dumps(outjson)
+
+	def getjoinedservers(self, usernm, passwd):
+		if not os.path.exists(serverStorage + os.sep + "users" + os.sep + usernm):
+			return '{"resp": false, "reason":"user does not exist"}'
+		
+		userinfo = self.openjson(serverStorage + os.sep + "users" + os.sep + usernm)
+
+		if not userinfo["passwd"] == passwd:
+			return '{"resp": false, "reason":"user password is incorrect"}'
+
+		outjson = json.loads("{}")
+		outjson["resp"] = True
+		outjson["servers"] = userinfo["joinedservers"]
+
+		return json.dumps(outjson)
+
+	def getprovidername(self):
+		providerinfo = json.loads("{}")
+		providerinfo["resp"] = True
+		providerinfo["name"] = providername
+		providerinfo["welcome"] = providerwelcome
+
+		return json.dumps(providerinfo)
 
 	def rawcommand(self, command):
 		if command == "":
@@ -181,26 +288,61 @@ class UserClient:
 			return "CLIENT UNGRACEFULLY CLOSED"
 		commandjson = json.loads(command)
 		call = commandjson["call"]
-		if call == "login":
-			return self.login(commandjson["usernm"], commandjson["passwd"])
-		elif call == "register":
-			return self.register(commandjson["usernm"], commandjson["passwd"])
-		elif call == "createserver":
-			return self.createserver(commandjson["name"], commandjson["passwd"], commandjson["owner"])
-		elif call == "joinserver":
-			return self.joinserver(commandjson["servername"], commandjson["serverpasswd"], commandjson["usernm"])
-		elif call == "createchannel":
-			return self.createchannel(commandjson["name"], commandjson["servername"], commandjson["usernm"])
-		elif call == "deleteserver":
-			return self.deleteserver(commandjson["servername"], commandjson["serverpasswd"], commandjson["usernm"], commandjson["passwd"])
-		elif call == "leaveserver":
-			return self.leaveserver(commandjson["servername"], commandjson["usernm"], commandjson["passwd"])
-		elif call == "message":
-			return self.message(commandjson["msg"], commandjson["servername"], commandjson["channel"], commandjson["usernm"], commandjson["passwd"])
-		else:
-			out = self.msg(command)
-			#print(out)
-			return out
+
+		if not (call == "login" or call == "register"):
+			if commandjson.get("usernm") == None:
+				return '{"resp": false, "reason":"usernm not in request"}'
+			if commandjson.get("passwd") == None:
+				return '{"resp": false, "reason":"passwd not in request"}'
+			if not os.path.exists(serverStorage + os.sep + "users" + os.sep + commandjson["usernm"]):
+				return '{"resp": false, "reason":"user does not exist"}'
+			userinfocheck = self.openjson(serverStorage + os.sep + "users" + os.sep + commandjson["usernm"])
+			if not userinfocheck["passwd"] == commandjson["passwd"]:
+				return '{"resp": false, "reason":"user password is incorrect"}'
+		try:
+			if call == "login":
+				return self.login(commandjson["usernm"], commandjson["passwd"])
+			elif call == "register":
+				return self.register(commandjson["usernm"], commandjson["passwd"])
+			elif call == "createserver":
+				return self.createserver(commandjson["servername"], commandjson["serverpasswd"], commandjson["owner"])
+			elif call == "joinserver":
+				return self.joinserver(commandjson["servername"], commandjson["serverpasswd"], commandjson["usernm"])
+			elif call == "createchannel":
+				return self.createchannel(commandjson["name"], commandjson["servername"], commandjson["usernm"])
+			elif call == "deleteserver":
+				return self.deleteserver(commandjson["servername"], commandjson["serverpasswd"], commandjson["usernm"], commandjson["passwd"])
+			elif call == "leaveserver":
+				return self.leaveserver(commandjson["servername"], commandjson["usernm"], commandjson["passwd"])
+			elif call == "message":
+				requesttimediff = datetime.datetime.now().timestamp() - self.lastgetmessagerequest
+				#print(requesttimediff)
+				self.lastgetmessagerequest = datetime.datetime.now().timestamp()
+				if requesttimediff > 0:
+					return self.message(commandjson["msg"], commandjson["servername"], commandjson["channel"], commandjson["usernm"], commandjson["passwd"])
+				else:
+					return '{"resp": false, "reason":"requesting too many times, only request every 0.75 seconds"}'
+			elif call == "getjoinedservers":
+				return self.getjoinedservers(commandjson["usernm"], commandjson["passwd"])
+			elif call == "getmessages":
+				return self.getmessages(commandjson["servername"], commandjson["channel"], commandjson["begin"], commandjson["end"], commandjson["usernm"])
+			elif call == "gettotalmessages":
+				requesttimediff = datetime.datetime.now().timestamp() - self.lastgetmessagetotalrequest
+				#print(requesttimediff)
+				self.lastgetmessagetotalrequest = datetime.datetime.now().timestamp()
+				if requesttimediff > 0.75:
+					return self.gettotalmessages(commandjson["servername"], commandjson["channel"])
+				else:
+					return '{"resp": false, "reason":"requesting too many times, only request every 0.75 seconds"}'
+			elif call == "getprovidername":
+				return self.getprovidername()
+			else:
+				out = self.msg(command)
+				print("UNKNOWN CALL:")
+				print(json.dumps(commandjson))
+				return out
+		except KeyError:
+			return '{"resp": false, "reason":"incorrect request values"}'
 
 	def msg(self, msg):
 		return str(msg)
@@ -215,6 +357,16 @@ class UserClient:
 		pathfile = open(path, "w")
 		json.dump(jsondata, pathfile, indent=jsonfileindent)
 		pathfile.close()
+
+	def sendotherusers(self, jsondata, servername):
+		if len(clients) > 1:
+			for i in range(0, len(clients)):
+				if not clients[i].usernm == self.usernm:
+					if servername in clients[i].joinedservers:
+						#print("sending")
+						jsontext = json.dumps(jsondata)
+						print("(" + getTime() + ") | " + serverNameInConsole + " ==> [" + str(addr[0]) + ":" + str(addr[1]) + "] | " + json.dumps(jsontext))
+						clients[i].clientsocket.send(jsontext.encode(encoding))
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
